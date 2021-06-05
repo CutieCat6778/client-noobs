c5c206c222464c2341f140b5b88b249412ce2c71
CutieCat6778 <thinhnguyenhuhung@gmail.com>
2021-06-05 19:23:59 +0200

    fixed the url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export function getUserDetails() {
-    return axios.get('https://noobs-map-backend.herokuapp.com//api/auth', {withCredentials: true})
+    return axios.get('https://noobs-map-backend.herokuapp.com/api/auth', {withCredentials: true})
 }
 
 export function logOut(){
-    return axios.get('https://noobs-map-backend.herokuapp.com//api/auth/logout', {withCredentials: true})
+    return axios.get('https://noobs-map-backend.herokuapp.com/api/auth/logout', {withCredentials: true})
 }
 
 export function updateUserLocation(query){
-    return axios.get(`https://noobs-map-backend.herokuapp.com//api/map/location?location=${query}`, {withCredentials: true})
+    return axios.get(`https://noobs-map-backend.herokuapp.com/api/map/location?location=${query}`, {withCredentials: true})
 }
