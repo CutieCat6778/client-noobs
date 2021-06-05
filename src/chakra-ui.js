0fc77fbeabc27705c791bebd86707e60b5957221
CutieCat6778 <thinhnguyenhuhung@gmail.com>
2021-06-05 17:24:31 +0200

    maybe fixed the webpack size

--- src/chakra-ui.js
+++ src/chakra-ui.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import App from './App';
+const App = React.lazy(() => import('./App'));
 import { ChakraProvider, CSSReset } from "@chakra-ui/react"
 import Loading from './components/loadingCircle';
 
 export function Chakra(){
     return(
-        <>
-            <ChakraProvider>
-                <React.Suspense fallback={<Loading/>}>
-                    <CSSReset />
-                        <App />
-                </React.Suspense>
-            </ChakraProvider>
-        </>
+        <ChakraProvider>
+            <CSSReset />
+            <React.Suspense fallback={<Loading/>}>
+                <App />
+            </React.Suspense>
+        </ChakraProvider>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Loading from './components/loadingCircle';
 const Chakra = React.lazy(() => import('./chakra-ui'));
 
 ReactDom.render(
-    <Chakra/>
+    <React.Suspense fallback={<Loading/>}>
+        <Chakra/>
+    </React.Suspense>
     , document.getElementById('root')
 );
